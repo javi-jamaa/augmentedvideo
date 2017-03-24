ba3de3c82fca3f6b50918ce002ee5dd4af6c8872
Guido García <guido.garciabernardo@telefonica.com>
2017-03-24 02:59:34 +0100

    chore: remove crap

--- config.js
+++ config.js
@@ -4,54 +4,7 @@ var config = {
   port: 3000,
   secure_port: 8443,
   apiBaseURL: 'http://127.0.0.1:3000/agora/api/v1/members/',
-  realm: 'mycompany',
   membersApiPageSize: 100,
-  clients: {
-    cleaner: {
-      secret: 'mysecret',
-      company: 'A'
-    },
-    test: {
-      secret: 'secretString',
-      company: 'B'
-    }
-  },
-  github: {
-    clientId: '',
-    clientSecret: '',
-    adminToken: '',
-    callbackURL: 'http://localhost:3000/agora/auth/github/callback',
-    orga: 'ouhyeah'
-  },
-  email: {
-    host: '',
-    port: 25,
-    from: 'agora@telefonica.com',
-    subject: 'A new member has joined github',
-    text: 'The github user {username} with validated email {email} has been invited to join your team {team} at https://github.com/{orga}'
-  },
-  companies: {
-    TID: {
-      team: 'Telefonica I+D',
-      adminEmail: '',
-      allowedDomains: ['gmail.com', 'telefonica.com'],
-      isUserValidService: {
-        url: '',
-        clientId: '',
-        clientSecret: ''
-      }
-    },
-    Tokbox: {
-      team: 'Tokbox',
-      adminEmail: '',
-      allowedDomains: ['tokbox.com']
-    },
-    MailCo: {
-      team: 'Otro',
-      adminEmail: '',
-      allowedDomains: ['gmail.com']
-    }
-  },
   userDatabase: './videos'
 };
 
